70f33d36d21323429c92774c90a1ef755a541632
connor <connor.jaksik@gmail.com>
2019-02-05 08:22:39 -0700

    Updates

---
+++ precache-manifest.6595a3cd5e3fd7471b001e2fd64014cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "7b76e3c1ffc67e050258",
+    "url": "/clicky-game/static/js/main.7b76e3c1.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "7b76e3c1ffc67e050258",
+    "url": "/clicky-game/static/css/main.8b8a8c9c.chunk.css"
+  },
+  {
+    "revision": "fd6290e40652c1deeb94a8047b6bb8d9",
+    "url": "/clicky-game/index.html"
+  }
+];
